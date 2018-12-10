c735d38f93bb99a69cdcb2e71170fdeb05d726aa
Tong Su (Contractor) <tsu@opentext.com>
2018-12-10 16:33:47 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import PIL
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+
+model = Sequential()
+model.add(Conv2D(32, 3, input_shape=(150, 150, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    r'.\img\all\train',
+    target_size=(150, 150),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    r'.\img\all\validation',
+    target_size=(150, 150),
+    batch_size=batch_size,
+    class_mode='binary')
+
+
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=2000,
+    epochs=50,
+    validation_data=validation_generator,
+    validation_steps=800)
+
+model.save_weights('first_try.h5')
